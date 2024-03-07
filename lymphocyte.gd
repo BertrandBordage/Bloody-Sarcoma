8eca747ca75248612c6b973e7494fd91538afab6
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-03-07 22:49:27 +0100

    Makes lymphocytes attracted to cancer cells.

--- lymphocyte.gd
+++ lymphocyte.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 
 const TYPE: String = "Lymphocyte"
+const SPEED: float = 1.5
 
 
 func take_damage(damage: float):
@@ -10,3 +11,17 @@ func take_damage(damage: float):
 
 func _on_health_component_died():
     queue_free()
+
+
+func _physics_process(delta):
+    var target_direction: Vector2 = Vector2.ZERO
+    var target_distance: float = INF
+    for body in %ThreadDetectionArea.get_overlapping_bodies():
+        if "TYPE" in body and body.TYPE == "CancerCell":
+            var direction: Vector2 = body.global_position - global_position
+            var distance: float = direction.length()
+            if distance < target_distance:
+                target_direction = direction
+                target_distance = distance
+    if target_direction != Vector2.ZERO:
+        apply_force(SPEED * target_direction.normalized())
