210400c8f315e77038d6c2bd1ef2efde896706cd
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-03-09 16:32:06 +0100

    Starts replacing the blood vessel mess with the tilemap.

--- blood_vessel.gd
+++ blood_vessel.gd
@@ -6,8 +6,6 @@ const MAX_SPAWNED = 500
 @export var spawnable_probabilities: Array[float]
 var spawn_exclusion_global_transform: Transform2D
 var spawn_exclusion_polygon: PackedVector2Array
-var entered_bodies = []
-var in_membrane_bodies = []
 @onready var curve: Curve2D = %Path2D.curve
 
 
@@ -43,25 +41,8 @@ func _process(_delta):
 
     for body in get_overlapping_bodies():
         if body is RigidBody2D:
-            if body not in in_membrane_bodies:
-                body.collision_mask |= 0b10000
             var offset = curve.get_closest_offset(body.global_position)
             var flow_rotation: float = curve.sample_baked_with_rotation(offset).get_rotation()
             body.apply_force(
                 Heartbeat.blood_pressure * Vector2.UP.rotated(flow_rotation)
             )
-
-
-func _on_body_exited(body):
-    if body is RigidBody2D:
-        body.collision_mask &= ~0b10000
-
-
-func _on_membrane_area_body_entered(body):
-    if body is RigidBody2D and body not in in_membrane_bodies:
-        in_membrane_bodies.append(body)
-
-
-func _on_membrane_area_body_exited(body):
-    if body in in_membrane_bodies:
-        in_membrane_bodies.erase(body)
