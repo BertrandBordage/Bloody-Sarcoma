2b26a249dca6d5018025f3971587fe82ddf9338b
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-03-10 13:02:23 +0100

    Process visible paths only.

--- autoload/SpawnedFlow.gd
+++ autoload/SpawnedFlow.gd
@@ -6,6 +6,7 @@ const PATH_SEARCH_OFFSET_INTERVAL: float = 10.0
 const VELOCITY_MULTIPLIER = 100.0
 var spawn_container: Node2D
 var spawned_bodies: Array[RigidBody2D] = []
+var flow_paths: Array[FlowPath] = []
 var flow_velocity: Vector2 = Vector2.ZERO
 var spawn_exclusion_global_position: Vector2
 var spawn_exclusion_polygon: PackedVector2Array
@@ -24,6 +25,51 @@ var lymphocyte_probability: float:
         spawnable_probabilities[0] = value
 
 
+# TODO: Spawn cells all AFTER the path as well. Ideally make the before/after decision based on current velocity.
+# TODO: Spawn non-player cancer cells depending on how many were dropped in a given area.
+
+
+class FlowPath:
+    var path: Path2D
+    var curve: Curve2D
+    var bounding_box: PackedVector2Array
+
+    func _init(init_path: Path2D):
+        path = init_path
+        curve = path.curve
+        bounding_box = get_bounding_box()
+
+    func get_bounding_box() -> PackedVector2Array:
+        var left: float = +INF
+        var right: float = -INF
+        var top: float = +INF
+        var bottom: float = -INF
+        var curve_transform: Transform2D = path.global_transform
+        for point in curve.get_baked_points():
+            point *= curve_transform
+            if point.x < left:
+                left = point.x
+            if point.x > right:
+                right = point.x
+            if point.y < top:
+                top = point.y
+            if point.y > bottom:
+                bottom = point.y
+        return PackedVector2Array([
+            Vector2(left, top),
+            Vector2(right, top),
+            Vector2(right, bottom),
+            Vector2(left, bottom),
+        ])
+
+    func is_visible() -> bool:
+        return Geometry2D.intersect_polygons(
+            bounding_box,
+            # FIXME: Replace with the loaded polygon, not the exclusion polygon.
+            SpawnedFlow.spawn_exclusion_polygon,
+        ).size() > 0
+
+
 func find_closest_point_outside_spawn_exclusion(curve: Curve2D, from_position: Vector2, offset: float):
     var is_start: bool = true
     var point: Vector2
@@ -40,7 +86,7 @@ func find_closest_point_outside_spawn_exclusion(curve: Curve2D, from_position: V
     return null
 
 
-func get_spawn_position(paths: Array[Node], initial: bool = false):
+func get_spawn_position(paths: Array, initial: bool = false):
     if initial:
         # TODO: Implement and call this on program start.
         pass
@@ -66,7 +112,7 @@ func get_spawn_position(paths: Array[Node], initial: bool = false):
         return point
 
 
-func spawn_random(paths: Array[Node], initial: bool = false):
+func spawn_random(paths: Array, initial: bool = false):
     if spawned_bodies.size() >= MAX_SPAWNED:
         return
 
@@ -77,5 +123,36 @@ func spawn_random(paths: Array[Node], initial: bool = false):
     var spawned = Math.choice(spawnable_scenes, spawnable_probabilities).instantiate()
     spawned.rotation = randf_range(-PI, PI)
     spawned.linear_velocity = flow_velocity * VELOCITY_MULTIPLIER
-    SpawnedFlow.spawn_container.add_child.call_deferred(spawned)
+    spawn_container.add_child.call_deferred(spawned)
     spawned.set_deferred("global_position", spawn_position)
+
+
+func move_bodies_in_flow(paths: Array) -> void:
+    for body in spawned_bodies:
+        var closest_curve: Curve2D
+        var closest_distance: float = INF
+
+        for path in paths:
+            var point = path.global_position + path.curve.get_closest_point(body.global_position)
+            var distance = (point - body.global_position).length()
+            if distance < closest_distance:
+                closest_curve = path.curve
+                closest_distance = distance
+
+        if closest_distance == INF:
+            continue
+
+        var offset = closest_curve.get_closest_offset(body.global_position)
+        var flow_rotation: float = closest_curve.sample_baked_with_rotation(offset).get_rotation()
+        body.apply_force(
+            Heartbeat.blood_pressure * Vector2.UP.rotated(flow_rotation)
+        )
+
+
+func _process(_delta):
+    var visible_paths = flow_paths.filter(
+        func (flow_path: FlowPath): return flow_path.is_visible()
+    ).map(func (flow_path: FlowPath): return flow_path.path)
+
+    spawn_random(visible_paths)
+    move_bodies_in_flow(visible_paths)
--- blood_vessel.gd
+++ blood_vessel.gd
@@ -1,28 +1,14 @@
 extends Node2D
 
 
-@onready var paths: Array[Node] = %Paths.get_children()
-
-# TODO: Spawn cells all AFTER the path as well. Ideally make the before/after decision based on current velocity.
-# TODO: Spawn non-player cancer cells depending on how many were dropped in a given area.
+var current_flow_paths: Array
 
 
-func _process(_delta):
-    SpawnedFlow.spawn_random(paths)
+func _ready():
+    current_flow_paths = %Paths.get_children().map(SpawnedFlow.FlowPath.new)
+    SpawnedFlow.flow_paths.append_array(current_flow_paths)
 
-    for body in SpawnedFlow.spawned_bodies:
-        var closest_curve: Curve2D
-        var closest_distance: float = INF
 
-        for path in paths:
-            var point = path.global_position + path.curve.get_closest_point(body.global_position)
-            var distance = (point - body.global_position).length()
-            if distance < closest_distance:
-                closest_curve = path.curve
-                closest_distance = distance
-
-        var offset = closest_curve.get_closest_offset(body.global_position)
-        var flow_rotation: float = closest_curve.sample_baked_with_rotation(offset).get_rotation()
-        body.apply_force(
-            Heartbeat.blood_pressure * Vector2.UP.rotated(flow_rotation)
-        )
+func _exit_tree():
+    for flow_path in current_flow_paths:
+        SpawnedFlow.flow_paths.erase(flow_path)
