4c57f966ab8eda54fb97c745f8f5cfcca571a7b1
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-03-10 14:41:54 +0100

    Starts chunking in an attempt to make the game scalable and run smoother.

---
+++ utils/tilemap_enabler.gd
@@ -0,0 +1,22 @@
+extends VisibleOnScreenNotifier2D
+
+
+@onready var parent: TileMap = get_parent()
+
+
+func toggle(enabled: bool):
+    parent.process_mode = PROCESS_MODE_INHERIT if enabled else PROCESS_MODE_DISABLED
+    for layer_index in range(parent.get_layers_count()):
+        parent.set_layer_enabled(layer_index, enabled)
+
+
+func _ready():
+    toggle(false)
+
+
+func _on_screen_entered():
+    toggle(true)
+
+
+func _on_screen_exited():
+    toggle(false)
