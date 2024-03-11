398de0567fbcaf799d5fd385a9c0031dcbf062cb
Bertrand Bordage <bordage.bertrand@gmail.com>
2024-03-11 21:56:17 +0100

    Removes a print.

--- GameOver.gd
+++ GameOver.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 
 
 func _on_game_over_restart_button_pressed():
-    print("game over restart")
     %GameOver.visible = false
     %GameOver.process_mode = PROCESS_MODE_DISABLED
     PlayerData.restart()
